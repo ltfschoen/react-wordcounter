7f41c470a93ca636d3f3829e86b86d0154b18dfd
Michael Bamford <michael@michaelbamford.com>
2017-11-27 17:00:29 +1100

    Create WordCounter

---
+++ hello.js
@@ -0,0 +1,16 @@
+function Adder({ n1, n2 }) {
+    const sum = n1 + n2;
+    return (
+        <h1>{sum}</h1>
+    )
+}
+
+// ReactDOM.render(
+//     React.createElement(Adder, { n1:4, n2:4 }),
+//         document.getElementById('app')
+// );
+
+ReactDOM.render(
+    <Adder n1={2} n2={4}/>,
+    document.getElementById('app')
+);
---
+++ index.js
@@ -0,0 +1,79 @@
+function Counter({ count }) {
+    return(
+        <p className="mb2">
+            Word Count: {count}
+        </p>
+    )
+}
+
+function ProgressBar({ completion }) {
+    const percentage = completion * 100;
+    return(
+        <div className="mv2 flex flex-column">
+            <label htmlFor="progress" className="mv2">
+                Progress
+            </label>
+            <progress value={completion} id="progress" className="bn">
+                {percentage}
+            </progress>
+        </div>
+    )
+}
+
+function Editor({ text, onTextChange }) {
+    function handleChange(event) {
+        onTextChange(event.target.value);
+    }
+
+    return(
+        <div className="flex flex-column mv2">
+            <label htmlFor="editor" className="mv2">
+                Enter your text:
+            </label>
+            <textarea value={text} onChange={handleChange} id="editor"/>
+        </div>
+    )
+}
+
+function countWords(text) {
+    return text ? text.match(/\w+/g).length : 0
+}
+
+class WordCounter extends React.Component {
+    constructor() {
+        super();
+        this.state = { text: '' }
+        // this.handleTextChange = this.handleTextChange.bind(this);
+    }
+
+    // handleTextChange(currentText) {
+    //     this.setState(() => { return { text: currentText }})
+    // }
+
+    // handleTextChange(currentText) {
+    //     this.setState(() => ({ text: currentText }))
+    // }
+
+    handleTextChange = (currentText) => {
+        this.setState({ text: currentText })
+    }
+
+    render() {
+        const { targetWordCount } = this.props;
+        const { text } = this.state;
+        const wordCount = countWords(text);
+        const progress = wordCount / targetWordCount;
+        return(
+            <form className="measure pa4 sans-serif">
+                <Editor text={text} onTextChange={this.handleTextChange}/>
+                <Counter count={wordCount}/>
+                <ProgressBar completion={progress}/>
+            </form>
+        )
+    }
+}
+
+ReactDOM.render(
+    <WordCounter targetWordCount={10}/>,
+    document.getElementById('app')
+)
